326707c78911f14eeb601a919ab7f3f05bd07549
Max Nichols <allenmaxnichols@gmail.com>
2025-08-27 17:30:39 +0300

    change calculations to get sat and path aligned

--- src/components/satellite/index.tsx
+++ src/components/satellite/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Card, CardTitle } from '@/components/ui/card';
-import { SatellitePosition } from '@/types/types';
 import * as satellite from 'satellite.js';
 import { Html } from '@react-three/drei';
 import { Line } from '@react-three/drei';
@@ -17,8 +16,8 @@ const satrec = satellite.twoline2satrec(tle_line1, tle_line2);
       const scale = 1 / 6371; // Earth radius in km
       positions.push([
         result.position.x * scale,
-        result.position.z * scale, // Z and Y are swapped for three.js
-        result.position.y * scale
+        result.position.y * scale, // Z and Y are swapped for three.js
+        result.position.z * scale
       ]);
     }
   }
@@ -26,8 +25,40 @@ const satrec = satellite.twoline2satrec(tle_line1, tle_line2);
 }
 
 
-export default function SatellitePath({ tle_line1, tle_line2 }: { tle_line1: string, tle_line2: string }) {
+export default function SatellitePath({ tle_line1, tle_line2, object_name }: { tle_line1: string, tle_line2: string, object_name: string }) {
   let points = getOrbitPath(tle_line1, tle_line2);
   if (points.length < 2) return null;
-  return <Line points={points} color="yellow" lineWidth={.5} />;
+
+  // Get Current Position
+  const satrec = satellite.twoline2satrec(tle_line1, tle_line2);
+  const now = new Date();
+  const result = satellite.propagate(satrec, now);
+  let satPos: [number, number, number] | null = null;
+  if(result?.position) {
+    const scale = 1 / 6371;
+    satPos = [
+      result.position.x * scale,
+      result.position.y * scale,
+      result.position.z * scale
+    ]
+  }
+
+  return (
+    <>
+      <Line points={points} color="yellow" lineWidth={.5} />
+      {satPos && (
+        <mesh position={satPos}>
+          <sphereGeometry args={[0.01, 10, 10]} />
+          <meshStandardMaterial color="red" />
+          {/* <Html>
+            <Card className='bg-black p-2' style={{ minWidth: "max-content" }}>
+              <CardTitle className='text-white'>
+                {object_name}
+              </CardTitle>
+            </Card>
+          </Html> */}
+        </mesh>
+      )}
+    </>
+);
 }
