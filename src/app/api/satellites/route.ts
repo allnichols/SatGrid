ca4cae24322c301b2931619b8e7602d0dc77d888
Max Nichols <allenmaxnichols@gmail.com>
2025-08-27 15:51:05 +0300

    add type for satellite

--- src/app/api/satellites/route.ts
+++ src/app/api/satellites/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import mysql from 'mysql2/promise';
+import { Satellite } from "./types";
 
-export async function GET(request: Request) {
+export async function GET() {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: process.env.DB_USER as string,
@@ -10,16 +11,17 @@ export async function GET(request: Request) {
     });
 
     try {
+
         const [rows] = await connection.execute(`
-                SELECT 
-                    satellites.object_name, 
-                    satellites.tle_line1, 
-                    satellites.tle_line2 
-                FROM 
-                    satellites;
-            `)
+            SELECT 
+                satellites.object_name, 
+                satellites.tle_line1, 
+                satellites.tle_line2 
+            FROM 
+                satellites;
+        `);
         await connection.end();
-        return NextResponse.json(rows, { status: 200 });
+        return NextResponse.json(rows as Satellite[], { status: 200 });
     } catch (error) {
         console.error('Error executing query:', error);
         await connection.end();
---
+++ src/app/api/satellites/types.ts
@@ -0,0 +1,5 @@
+export type Satellite = {
+    object_name: string;
+    tle_line1: string;
+    tle_line2: string;
+};
