ac9fd5ad6d26c328cc8ec3ddc294650868562d02
Max Nichols <allenmaxnichols@gmail.com>
2025-08-31 17:05:34 +0300

    add meta type

--- src/app/api/satellites/types.ts
+++ src/app/api/satellites/types.ts
@@ -5,3 +5,23 @@ export type TSatellite = {
     category: string;
     subcategory?: string;
 };
+
+
+export type SatelliteMeta = {
+      satellite_id: number;
+      timestamp: string;
+      latitude: number;
+      longitude: number;
+      altitude_km: number;
+      velocity_kms: number | undefined;
+      object_name: string;
+      norad_cat_id: string;
+      sub_category: string;
+      category: string;
+      inclination: number;
+      ra_of_asc_node: number;
+      eccentricity: number;
+      arg_of_pericenter: number;
+      mean_anomaly: number;
+      mean_motion: number;
+  };
