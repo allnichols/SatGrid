0c1d6a81a68dec58e98d2e77876c3610c81295c2
Max Nichols <allenmaxnichols@gmail.com>
2025-08-31 18:10:28 +0300

    chagne type and add attributes

--- src/app/api/satellite_positions/types.ts
+++ src/app/api/satellite_positions/types.ts
@@ -8,20 +8,26 @@ export type TSatellite = {
 
 
 export type SatelliteMeta = {
-      satellite_id: number;
-      timestamp: string;
-      latitude: number;
-      longitude: number;
-      altitude_km: number;
-      velocity_kms: number | undefined;
-      object_name: string;
-      norad_cat_id: string;
-      sub_category: string;
-      category: string;
-      inclination: number;
-      ra_of_asc_node: number;
-      eccentricity: number;
-      arg_of_pericenter: number;
-      mean_anomaly: number;
-      mean_motion: number;
-  };
+    id: number;
+    object_id: string;
+    object_name: string;
+    norad_cat_id: number;
+    tle_line1: string;
+    tle_line2: string;
+    category: string;
+    sub_category: string;
+    classification_type: string;
+    epoch: string;
+    element_set_no: number;
+    inclination: number;
+    ra_of_asc_node: number;
+    eccentricity: number;
+    arg_of_pericenter: number;
+    mean_anomaly: number;
+    mean_motion: number;
+    mean_motion_dot: number;
+    mean_motion_ddot: number;
+    rev_at_epoch: number;
+    ephemeris_type: number;
+    bstar: number;
+};
--- src/components/info-card/index.tsx
+++ src/components/info-card/index.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Card, CardTitle } from '../ui/card';
+import { Card, CardContent, CardTitle } from '../ui/card';
 import { useGetMetaDataQuery } from '@/services/api';
 import { RootState } from '@/lib/store';
 
@@ -11,7 +11,7 @@ export default function InfoCard() {
     }
 
     return (
-        <Card className="absolute top-4 right-4">
+        <Card className="bg-black text-white p-4 absolute bottom-4 right-4">
            <InfoCardContent selectedSatellite={selectedSatellite} />
         </Card>
     )
@@ -28,9 +28,28 @@ function InfoCardContent({ selectedSatellite }: { selectedSatellite: string }) {
         return <div>Error loading satellite data</div>;
     }
 
+    console.log(data);
+
     return (
         <>
             <CardTitle>{data?.[0].object_name}</CardTitle>
+            <CardContent className='pl-0'>
+                <p className=""><span className="font-semibold">Satellite Name: </span>{data?.[0].object_name}</p>
+                <p className=""><span className="font-semibold">NORAD ID: </span>{data?.[0].norad_cat_id}</p>
+                <p className=""><span className="font-semibold">TLE Line 1: </span>{data?.[0].tle_line1}</p>
+                <p className=""><span className="font-semibold">TLE Line 2: </span>{data?.[0].tle_line2}</p>
+                <p className=""><span className="font-semibold">Mean Motion: </span>{data?.[0].mean_motion}</p>
+                <p className=""><span className="font-semibold">Mean Motion Dot: </span>{data?.[0].mean_motion_dot}</p>
+                <p className=""><span className="font-semibold">Mean Motion Ddot: </span>{data?.[0].mean_motion_ddot}</p>
+                <p className=""><span className="font-semibold">Eccentricity: </span>{data?.[0].eccentricity}</p>
+                <p className=""><span className="font-semibold">Inclination: </span>{data?.[0].inclination}</p>
+                <p className=""><span className="font-semibold">RA of Asc Node: </span>{data?.[0].ra_of_asc_node}</p>
+                <p className=""><span className="font-semibold">Arg of Pericenter: </span>{data?.[0].arg_of_pericenter}</p>
+                <p className=""><span className="font-semibold">Mean Anomaly: </span>{data?.[0].mean_anomaly}</p>
+                <p className=""><span className="font-semibold">Rev at Epoch: </span>{data?.[0].rev_at_epoch}</p>
+                <p className=""><span className="font-semibold">Ephemeris Type: </span>{data?.[0].ephemeris_type}</p>
+                <p className=""><span className="font-semibold">Bstar: </span>{data?.[0].bstar}</p>
+            </CardContent>
         </>
 
     );
