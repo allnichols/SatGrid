a16abe00036b26a08ce6e44a2da8a18027902f07
Max Nichols <allenmaxnichols@gmail.com>
2025-09-02 12:29:22 +0300

    update target

--- src/components/orbit-controls/index.tsx
+++ src/components/orbit-controls/index.tsx
@@ -1,9 +1,23 @@
 import { OrbitControls } from "@react-three/drei";
 import { useSelector } from "react-redux";
 import { RootState } from "@/lib/store";
-import { Vector3 } from "@react-three/fiber";
+import { useFrame, Vector3, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
     
 export default function OrbitalControls() {
   const target = useSelector((state: RootState) => state.satellite.coordinates);
-  return <OrbitControls target={target as Vector3 || [0, 0, 0]}  />;
+  const controlsRef = useRef<any>(null);
+  const { camera } = useThree();
+
+  useFrame(() => {
+    // Smoothly interpolate the camera position towards the target
+    if (controlsRef.current) {
+      controlsRef.current.target.lerp(new THREE.Vector3(...target), 0.1);
+    
+      controlsRef.current.update();
+
+    }
+  });
+  return <OrbitControls ref={controlsRef} />;
 }
