518a28882e4ab70dc4b1c982ccf84545b837e505
Max Nichols <allenmaxnichols@gmail.com>
2025-09-03 16:02:49 +0300

    try dasiyui

--- src/app/api/search-satellites/route.ts
+++ src/app/api/search-satellites/route.ts
@@ -11,19 +11,19 @@ export async function GET(request: Request) {
 
     const searchParams = new URL(request.url).searchParams;
     const searchTerm = searchParams.get('searchTerm') || '';
-    const category = searchParams.get('category') || '';
-
+    const categoryParam = searchParams.get('category') || '';
+    const categories = categoryParam ? categoryParam.split(',').map(c => c.trim().toLowerCase()).filter(Boolean) : [];
+    
     try {
         let sql = `SELECT satellites.object_name, satellites.category FROM satellites WHERE LOWER(satellites.object_name) LIKE ?`
         let params: string[] = [`%${searchTerm.toLowerCase()}%`];
 
-        if(category !== ''){
-            sql += ` AND LOWER(satellites.category) = ?`;
-            params.push(category.toLowerCase());
+        if(categories.length > 0) {
+            sql += ` AND LOWER(satellites.category) IN (${categories.map(() => '?').join(',')})`;
+            params.push(...categories);
         }
 
         const [rows] = await connection.execute(sql, params);
-        console.log(rows)
         await connection.end();
         return NextResponse.json(rows);
     } catch (error) {
--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,9 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Input } from '@/components/ui/input';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Checkbox } from '@/components/ui/checkbox';
-import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
 import { useSearchSatellitesQuery } from '@/services/api';
 
 const categoryOptions = ['weather', 'communication', 'navigation'];
@@ -23,50 +18,46 @@ export default function SearchBar() {
 
     const handleCategoryChange = (category: string) => {
         setCategories(prev => {
-          return prev.includes(category)
-            ? prev.filter(cat => cat !== category)
-            : [...prev, category]
+            return prev.includes(category)
+                ? prev.filter(cat => cat !== category)
+                : [...prev, category]
         })
     }
 
-    console.log(categories)
-
     const { data, isError, isLoading, } = useSearchSatellitesQuery(
-        { searchTerm: debouncedSearchTerm, category: categories }, 
+        { searchTerm: debouncedSearchTerm, category: categories },
         { skip: debouncedSearchTerm.length < 1 });
 
     return (
-        <div className='bg-[#171717] absolute top-4 left-4 z-10 border-[1px] border-gray-800 rounded-md p-4 sm:width-[100%]'>
+        <div className='card w-96 bg-base-100 card-xs shadow-sm absolute top-4 left-4 z-10 border-[1px] border-gray-800 rounded-md p-4 sm:width-[100%]'>
             <div className='flex flex-row gap-2 justify-between'>
-                <Input
-                    type="text"
-                    placeholder="Search for satellites"
-                    className='text-white'
-                    aria-label="Search for satellites"
-                    onChange={e => setSearchTerm(e.target.value)}
-                />
-                <Popover>
-                    <PopoverTrigger>
+                <input type="text" placeholder="Medium" className="input input-md" aria-label="Search for satellites"
+                    onChange={e => setSearchTerm(e.target.value)} />
+
+                <div className="dropdown dropdown-center">
+                    <div tabIndex={0} role="button" className="btn">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                         </svg>
-                    </PopoverTrigger>
-                    <PopoverContent className='bg-[#171717] border border-gray-800 rounded-md p-4'>
-                        {categoryOptions.map(category => {
+                    </div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                         {categoryOptions.map(category => {
                             return (
                                 <div className="flex items-center mt-2 p-1" key={category}>
-                                    <Checkbox 
-                                        id={category} 
-                                        checked={categories.includes(category)} 
-                                        onCheckedChange={() => handleCategoryChange(category)}
-                                    />
-                                    <Label htmlFor={category} className='text-white ms-2'>{category.charAt(0).toUpperCase() + category.slice(1)}</Label>
+                                    <label className="label" htmlFor={category}>
+                                        <input
+                                            type="checkbox"
+                                            id={category}
+                                            className="checkbox"
+                                            checked={categories.includes(category)}
+                                            onChange={() => handleCategoryChange(category)} />
+                                        {category.charAt(0).toUpperCase() + category.slice(1)}
+                                    </label>
                                 </div>
                             )
                         })}
-                    </PopoverContent>
-                </Popover>
-
+                    </ul>
+                </div>
             </div>
             {isLoading && <p className='text-white'>Loading...</p>}
             {isError && <p className='text-white'>Error fetching data</p>}
