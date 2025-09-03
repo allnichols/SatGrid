6348ff853393ce8ba4896a107b0804e96829f38c
Max Nichols <allenmaxnichols@gmail.com>
2025-09-03 09:37:09 +0300

    await params

--- src/app/api/meta/[satelliteName]/route.ts
+++ src/app/api/meta/[satelliteName]/route.ts
@@ -2,15 +2,16 @@ import { NextResponse, } from "next/server";
 import mysql from 'mysql2/promise';
 import { SatelliteMeta } from "@/types/types";
 
-export async function GET(request: Request, { params }: { params: { satelliteName: string } }) {
+export async function GET(request: Request, context: { params:  any }) {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
     });
-    const { satelliteName } = params;
-    
+    const params = await context.params;
+    const satelliteName = params.satelliteName as string;
+
     try {
         const [rows] = await connection.execute(`
             SELECT * FROM satellites WHERE satellites.object_name = ?
