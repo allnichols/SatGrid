34e77b2eb7217381b675cc465e1fc55a0743aecc
Max Nichols <allenmaxnichols@gmail.com>
2025-09-08 09:35:33 +0300

    add charts btn to toolbar

--- src/components/header/toolbar/index.tsx
+++ src/components/header/toolbar/index.tsx
@@ -1,28 +1,14 @@
 import DeselectSatelliteBtn from "@/components/header/toolbar/components/deselect";
 import DetailsBtn from "@/components/header/toolbar/components/details";
+import ChartsBtn from "@/components/header/toolbar/components/charts";
 
 export default function Toolbar() {
     return (
-        <div className="absolute bottom-5 z-10 md:top-4">
+        <div className="absolute bottom-5 z-10 h-16 sm:top-4">
             <ul className="menu menu-horizontal bg-base-200 rounded-box mt-6">
                 <DeselectSatelliteBtn/>
                 <DetailsBtn />
-                <li>
-                    <a className="tooltip md:tooltip-bottom" data-tip="Stats">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                        </svg>
-                    </a>
-                </li>
+                <ChartsBtn />
             </ul>
         </div>
     );
--- src/lib/toolbarSlice.ts
+++ src/lib/toolbarSlice.ts
@@ -3,13 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const toolbarSlice = createSlice({
     name: 'toolbar',
-    initialState: { isDetailsOpen: false },
+    initialState: { isDetailsOpen: false, isChartsOpen: false },
     reducers: {
         toggleDetails: (state) => {
             state.isDetailsOpen = !state.isDetailsOpen;
         },
+        toggleCharts: (state) => {
+            state.isChartsOpen = !state.isChartsOpen;
+        },
     },
 });
 
-export const { toggleDetails } = toolbarSlice.actions;
+export const { toggleDetails, toggleCharts } = toolbarSlice.actions;
 export default toolbarSlice.reducer;
