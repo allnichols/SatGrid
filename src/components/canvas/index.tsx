473b6e40723e6efb77e64aa9da46894b82bf5be3
Max Nichols <allenmaxnichols@gmail.com>
2025-09-09 12:18:13 +0300

    minor changes

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -4,13 +4,17 @@ import SatelliteContainer from '@/components/canvas/satellite'
 import OrbitalControls from '@/components/canvas/orbit-controls';
 
 export default function CanvasScene() {
+
     return (
-        <Canvas style={{ height: "100vh" }}>
-            <ambientLight intensity={5} />
-            <directionalLight position={[3,3,3]} />
-            <Earth />
-            <SatelliteContainer />
-            <OrbitalControls />
-        </Canvas>
+        <div className='relative w-full h-full'>
+            <Canvas style={{ height: "100vh" }}>
+                <ambientLight intensity={5} />
+                <directionalLight position={[3, 3, 3]} />
+                <Earth />
+                <SatelliteContainer />
+                <OrbitalControls />
+            </Canvas>
+
+        </div>
     )
 }
